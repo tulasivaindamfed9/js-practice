2ed185b87aee7513ad5fc5d2a7431294fec7143c
tulasivaindamfed9 <tulasivaindamfed9@gmail.com>
2025-01-27 13:44:05 +0530

    dom book my show dummy app

---
+++ day9 Starting react/moviefile.js
@@ -0,0 +1,11 @@
+// creating movie object
+const movie1 = {
+    title: "Kalki",
+    time: "10AM",
+    seats: 60
+    };
+const movie2 = {
+    title: "Pushpa 2",
+    time: "3PM",
+    seats: 60
+    };
